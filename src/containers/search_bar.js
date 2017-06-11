dbe86b5a0df2a3f8fbbd3278a29e6eb25ead959d
Szalbik <szalbierz.d.k@gmail.com>
2017-06-11 15:55:04 +0200

    Add clock

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { fetchWeather } from '../actions/index';
+import Clock from '../components/clock';
 
 class SearchBar extends Component {
   constructor(props) {
@@ -29,17 +30,21 @@ class SearchBar extends Component {
 
   render() {
     return(
-      <form onSubmit={this.onFormSubmit} className="input-group">
-        <input
-          placeholder="Get a five-day forecast in your favorite cities"
-          className="form-control"
-          value={this.state.term}
-          onChange={this.onInputChange}
-          />
-        <span className="input-group-btn">
-          <button type="submit" className="btn btn-secondary">Submit</button>
-        </span>
-      </form>
+      <div>
+        <form onSubmit={this.onFormSubmit} className="input-group">
+          <input
+            placeholder="Get a five-day forecast in your favorite cities"
+            className="form-control"
+            value={this.state.term}
+            onChange={this.onInputChange}
+            />
+          <span className="input-group-btn">
+            <button type="submit" className="btn btn-secondary">Submit</button>
+          </span>
+        </form>
+        <Clock />
+      </div>
+
     );
   }
 }
